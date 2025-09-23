8b2a152c62ed46f38e61774d68373c25cc1fb7e4
eric.site <goole910805@gmail.com>
2025-09-23 15:36:39 +0800

    通知會消失

---
+++ release.ps1
@@ -0,0 +1,17 @@
+
+# 1. Git add
+git add .
+
+# 2. Git commit
+git commit -m $msg
+
+# 3. Git push
+git push origin main2
+
+# 4. Build
+npm run build
+
+# 5. Deploy
+npm run deploy
+
+Write-Host "✅ 發布完成！" -ForegroundColor Green
